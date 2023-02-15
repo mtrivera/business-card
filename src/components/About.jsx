812aec7b4f77cd0f06f8c05b4b87c87ca226f502
Miguel T Rivera <mrivera108@outlook.com>
2023-02-14 23:56:56 -0800

    Add content to about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,13 @@
 export default function About() {
-  return <h1>About section content!</h1>;
+  return (
+    <>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ante elit,
+        viverra id eros id, condimentum venenatis sem. Pellentesque iaculis
+        ultrices tortor, vel varius ipsum rhoncus et. In commodo in ligula nec
+        tempus.
+      </p>
+    </>
+  );
 }
