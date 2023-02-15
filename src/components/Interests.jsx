d86e40be88be35419d76ab0e87ab874f868cdf76
Miguel T Rivera <mrivera108@outlook.com>
2023-02-14 23:57:57 -0800

    Add content to interests component

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,3 +1,13 @@
 export default function Interests() {
-  return <h1>Interests section content!</h1>;
+  return (
+    <>
+      <h2>Interests</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ante elit,
+        viverra id eros id, condimentum venenatis sem. Pellentesque iaculis
+        ultrices tortor, vel varius ipsum rhoncus et. In commodo in ligula nec
+        tempus.
+      </p>
+    </>
+  );
 }
