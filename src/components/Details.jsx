a85163859b8f140fc0ad8793dfb9e47c8fc05d79
Miguel T Rivera <mrivera108@outlook.com>
2023-02-14 23:57:14 -0800

    Add content to details component

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,3 +1,12 @@
+import "../App.css";
+
 export default function Details() {
-  return <h1>Details section content!</h1>;
+  return (
+    <div className="details-content">
+      <img src="https://via.placeholder.com/317" />
+      <p>Miguel T Rivera</p>
+      <p>Software Developer</p>
+      <small>mtrivera.github.io</small>
+    </div>
+  );
 }
