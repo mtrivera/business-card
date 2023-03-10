e984ec75978369b3cf86a6ea379fc1c1fa4b03c2
Miguel T Rivera <mrivera108@outlook.com>
2023-03-09 23:54:55 -0800

    Add social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+
 export default function Footer() {
   return (
     <>
       <h2>Footer section!!</h2>
+      <FontAwesomeIcon icon={brands("square-twitter")} />
+      <FontAwesomeIcon icon={brands("square-github")} />
     </>
   );
 }
